f824c58b56f719e73e8214a4184b3f4378e1c9fe
yuq_laptop_x1 <yuq_laptop_x1@qq.com>
2024-02-18 15:52:56 +0800

    环境修改

--- basic_demo/web_demo_gradio.py
+++ basic_demo/web_demo_gradio.py
@@ -38,7 +38,8 @@ from transformers import (
 ModelType = Union[PreTrainedModel, PeftModelForCausalLM]
 TokenizerType = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
 
-MODEL_PATH = os.environ.get('MODEL_PATH', 'THUDM/chatglm3-6b')
+# MODEL_PATH = os.environ.get('MODEL_PATH', 'THUDM/chatglm3-6b')
+MODEL_PATH = os.environ.get('MODEL_PATH', 'E:\\Development\\ChatGLM\\model\\chatglm3-6b')
 TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH", MODEL_PATH)
 
 
